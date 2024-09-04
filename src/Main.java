a581df4923c2ee2eff6e86f8206803c80752709c
McGrzegor <grzegorz77.ku@gmail.com>
2024-09-04 13:40:35 +0200

    losowanie liczby rzeczywsitej

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("Witaj na leckcji");
+        double wylosowana = Math.random()*10+1;
+        System.out.println(wylosowana);
     }
 }
