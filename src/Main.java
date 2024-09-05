0c5f6d4b553136307763dcd9c0f3f24f956be7ca
McGrzegor <grzegorz77.ku@gmail.com>
2024-09-05 08:06:53 +0200

    kjhgv

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,5 +7,27 @@ public class Main {
         System.out.println("Witaj na leckcji");
         int wylosowana =(int) (Math.random()*10+1);
         System.out.println(wylosowana);
+        /*
+        typy proste z małej litery, mają tylko wartość
+        logiczny: boolean
+        całkowite: byte, short, int, long
+        rzeczywiste: float, double
+        znakowy: char
+
+        typy złożone zawsze pisane wielką literą
+        obiekty mają własności i metody
+
+         */
+        System.out.println("Zgadnij licznę z zakresu 1 do 10");
+        Scanner klawiatura = new Scanner(System.in);
+        int wpisanaLiczba = klawiatura.nextInt();//wczytwanie z klawiatury
+        System.out.println("Wpisana z klawiatury"+wpisanaLiczba);
+        if (wpisanaLiczba == wylosowana){
+            System.out.println("Trafiona Liczba");
+        }
+        else {
+            System.out.println("Nie trafiono spróbuj ponownie");
+        }
+
     }
 }
